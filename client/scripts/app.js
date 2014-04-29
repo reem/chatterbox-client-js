db8ca485f270b3a111f17f866f16aef4307c448b
Hack Reactor Students <jonathan.reem@gmail.com>
2014-04-28 17:12:33 -0700

    Added render mini-module.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,50 @@
-// YOUR CODE HERE:
+var $ = $;
+var _ = _;
 
+var render = (function () {
+  // Library of functions that create DOM nodes from
+  // our javascript classes.
+  var renderMessage = function (messageToRender) {
+    return messageToRender.user + ': ' + messageToRender.text;
+  };
+
+  var renderUser = function (userToRender) {
+    return userToRender.name;
+  };
+
+  var renderRoom = function (roomToRender) {
+    var $room = $('<ul></ul>');
+
+    $.each(roomToRender.messages, function (ind, messageToRender) {
+      $('<li/>').text(renderMessage(messageToRender)).appendTo($room);
+    });
+
+    return $room;
+  };
+
+  return {
+    message: renderMessage,
+    user: renderUser,
+    room: renderRoom,
+  };
+}());
+
+var app = (function () {
+  // Handles the overall logic and putting stuff on screen
+
+
+  var init = function () {
+    var main = new room.PublicRoom("main");
+    setInterval(function () {
+      $("#main").find("ul").remove();
+      $("#main").append(render.room(main));
+    }, 2000);
+  };
+
+  return {
+    init: init
+  };
+}());
+
+// Actual entrance point.
+app.init();
